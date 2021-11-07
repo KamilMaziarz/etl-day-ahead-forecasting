632be084e5f29afe887f94ce0050a2d134ac088c
KamilMaziarz <kamilmaziarz1995@gmail.com>
2021-11-07 17:21:51 +0100

    change logging setup

--- etl_day_ahead_forecasting/_settings.py
+++ etl_day_ahead_forecasting/_settings.py
@@ -1,16 +1,8 @@
 import logging.config
-from pathlib import Path
 
 import coloredlogs
-import yaml
 
-
-def setup_logging():
-    Path('logs').mkdir(parents=True, exist_ok=True)
-    with open('logger_config.yml', 'rt') as f:
-        config = yaml.safe_load(f.read())
-        logging.config.dictConfig(config)
-        coloredlogs.install()
-
-
-setup_logging()
+logging.config.fileConfig(fname='logger.conf', disable_existing_loggers=False)
+coloredlogs.install()
+logger = logging.getLogger(__name__)
+logger.info('Logger has been initiated.')
