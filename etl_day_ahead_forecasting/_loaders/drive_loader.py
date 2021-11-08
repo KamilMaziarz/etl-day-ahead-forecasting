9b2b33873b96a0d2fd1832b370768365a353bc28
KamilMaziarz <kamilmaziarz1995@gmail.com>
2021-11-08 20:51:50 +0100

    refactor drive loader

--- etl_day_ahead_forecasting/_loaders/drive_loader.py
+++ etl_day_ahead_forecasting/_loaders/drive_loader.py
@@ -1,17 +1,13 @@
 import pickle
 
-from etl_day_ahead_forecasting._pipeline.etl_pipeline import (  # noqa
-    PipelineStep,
-    ETLPropertiesPathWithDataType,
-    ETLPipelineData,
-)
-from etl_day_ahead_forecasting._utils.paths import get_resources_path  # noqa
+from etl_day_ahead_forecasting._pipeline._etl_pipeline_models import ETLPropertiesLocalSave  # noqa
+from etl_day_ahead_forecasting._pipeline.etl_pipeline import PipelineStep, ETLPipelineData  # noqa
 
 __all__ = ('DriveLoader',)
 
 
-class DriveLoader(PipelineStep[ETLPropertiesPathWithDataType]):
-    def execute(self, properties: ETLPropertiesPathWithDataType, data: ETLPipelineData) -> ETLPipelineData:
+class DriveLoader(PipelineStep[ETLPropertiesLocalSave]):
+    def execute(self, properties: ETLPropertiesLocalSave, data: ETLPipelineData) -> ETLPipelineData:
         if data is not None:
             with open(properties.path, 'wb') as file:
                 pickle.dump(data[properties.data_name], file)
