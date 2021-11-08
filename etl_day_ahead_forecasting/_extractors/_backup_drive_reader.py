3ba239f787ebea31165a749176b879a2717402c3
KamilMaziarz <kamilmaziarz1995@gmail.com>
2021-11-08 20:57:15 +0100

    separate read and save properties

--- etl_day_ahead_forecasting/_extractors/_backup_drive_reader.py
+++ etl_day_ahead_forecasting/_extractors/_backup_drive_reader.py
@@ -1,14 +1,14 @@
 import pickle
 import typing as t
 
-from etl_day_ahead_forecasting._pipeline._etl_pipeline_models import ETLPropertiesLocalSave  # noqa
+from etl_day_ahead_forecasting._pipeline._etl_pipeline_models import ETLPropertiesLocalRead  # noqa
 from etl_day_ahead_forecasting._pipeline.etl_pipeline import PipelineStep, ETLPipelineData  # noqa
 
 __all__ = ('BackupDriveReader',)
 
 
-class BackupDriveReader(PipelineStep[ETLPropertiesLocalSave]):
-    def execute(self, properties: ETLPropertiesLocalSave, data: t.Optional[ETLPipelineData] = None) -> ETLPipelineData:
+class BackupDriveReader(PipelineStep[ETLPropertiesLocalRead]):
+    def execute(self, properties: ETLPropertiesLocalRead, data: t.Optional[ETLPipelineData] = None) -> ETLPipelineData:
         with open(properties.path, 'rb') as file:
             extracted = pickle.load(file)
         return {properties.data_name: extracted}
