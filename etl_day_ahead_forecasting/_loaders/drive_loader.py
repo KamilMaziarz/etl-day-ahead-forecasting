45f6a61bac9cc341b4df6ff36e35fed9d2bb38c5
KamilMaziarz <kamilmaziarz1995@gmail.com>
2021-11-18 21:50:59 +0100

    add a few logs

--- etl_day_ahead_forecasting/_loaders/drive_loader.py
+++ etl_day_ahead_forecasting/_loaders/drive_loader.py
@@ -1,9 +1,12 @@
+import logging
 import pickle
 
 from etl_day_ahead_forecasting.pipeline._etl_pipeline import PipelineStep, _ETLPipelineData  # noqa
 from etl_day_ahead_forecasting.pipeline.models import _ETLPropertiesSaveLocal  # noqa
 
 __all__ = ('DriveLoader',)
+
+logger = logging.getLogger(__name__)
 
 
 class DriveLoader(PipelineStep[_ETLPropertiesSaveLocal]):
@@ -12,4 +15,5 @@ class DriveLoader(PipelineStep[_ETLPropertiesSaveLocal]):
             properties.path.parent.mkdir(parents=True, exist_ok=True)
             with open(properties.path, 'wb') as file:
                 pickle.dump(data[properties.local_data_type], file)
+            logger.info(f'SAVED to path: {properties.path}')
         return {}
